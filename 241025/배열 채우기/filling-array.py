a59602eb13eb392992d2a9a9629a544c0da8fa20
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2024-10-25 18:40:45 +0900

    Title: 배열 채우기, Time: 184ms, Memory: 24MB, Status: Passed - CodeTree

--- 241025/배열 채우기/filling-array.py
+++ 241025/배열 채우기/filling-array.py
@@ -1,6 +1,10 @@
 arr = list(map(int, input().split()))
-reverse_arr = arr[::-1]
+cnt = 0
 
-for i in reverse_arr :
-    if(i != 0) :
-        print(i,end = " ")
+for i in arr :
+    if i == 0:
+        break
+    cnt += 1
+
+for i in range(cnt -1, -1, -1):
+    print(arr[i],end = " ")
