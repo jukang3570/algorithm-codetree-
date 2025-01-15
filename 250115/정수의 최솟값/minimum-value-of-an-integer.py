a69bc1be6945a46c2fe6dcc8a76741d8ee6feb46
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-01-15 16:45:48 +0900

    Title: 정수의 최솟값, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/정수의 최솟값/minimum-value-of-an-integer.py
+++ 250115/정수의 최솟값/minimum-value-of-an-integer.py
@@ -1,10 +1,10 @@
 def ans (a = 0,b = 0,c = 0) :
     min_ans = -101
-    if a < b and a < c :
+    if a <= b and a <= c :
         min_ans = a
-    elif b < a and b < c :
+    elif b <= a and b <= c :
         min_ans = b
-    elif c < a and c < b:
+    elif c <= a and c <= b:
         min_ans = c
     return min_ans
 a, b, c = map(int, input().split())
