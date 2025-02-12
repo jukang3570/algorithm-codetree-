c1a7632e5324c4f83af2365fe4ea2deffdba73ae
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-12 17:01:30 +0900

    Title: 일렬로 서있는 소 2, Time: 70ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250212/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,14 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+# Write your code here!
+cnt = 0
+for i in range(N) :
+    state = A[i]
+    for j in range(i+1, N) :
+        if A[j] > state :
+            for k in range(j+1, N) :
+                if A[k] > A[j] :
+                    cnt += 1
+
+print(cnt)
