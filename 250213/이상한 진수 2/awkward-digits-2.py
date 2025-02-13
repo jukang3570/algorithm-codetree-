1c0d4c9fbaf5d88673d3a5ba87b695a71c0c33be
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-13 23:56:55 +0900

    Title: 이상한 진수 2, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250213/이상한 진수 2/awkward-digits-2.py
+++ 250213/이상한 진수 2/awkward-digits-2.py
@@ -2,15 +2,20 @@
 a = list(map(int,input()))
 ans = 0
 cnt = 0
+check = True
 for i in range(len(a)) :
     if a[i] == 0:
         a[i] = 1
+        check = False
         break
+if check == True :
+    for i in range(len(a)-1, -1, -1) :
+        if a[i] == 1:
+            a[i] = 0
+            break
 
 for i in range(len(a)-1, -1, -1) :
     ans += a[i] * (pow(2,cnt))
     cnt += 1
-if len(a) < 2 and a[i] == 1:
-    print(0)
-else:
-    print(ans)
+
+print(ans)
