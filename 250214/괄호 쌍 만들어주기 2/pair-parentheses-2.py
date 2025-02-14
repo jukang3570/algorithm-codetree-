0d21aaf3f97a6a0760a1883b4b2c234f5db7d5f7
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-14 15:26:57 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 250214/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,6 +1,6 @@
 arr = list(input())
 ans = 0
-for i in range(len((arr))) :
+for i in range(len((arr))-1) :
     if arr[i] == '(' and arr[i+1] == '(':
         for j in range(len(arr)-1, i, -1) :
             if arr[j] == ')' and arr[j-1] == ')' :
