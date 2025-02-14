e032ca12a47ff3b73cec23a5dc706bd932d91a70
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-14 14:53:10 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250214/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250214/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -1,27 +1,27 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력
 n = int(input())
-arr = [list(map(int,input().split())) for _ in range(n)]
+arr = [
+	list(map(int, input().split()))
+	for _ in range(n)
+]
 
-end_point = (arr[n-1][0], arr[n-1][1])
-cnt = 0
-ans = []
-value = 0
-state = [arr[0][0], arr[0][1]]
+# 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+ans = INT_MAX
+for i in range(1, n - 1):
+	# 거리를 구합니다.
+	dist = 0
+	prev_idx = 0
+	for j in range(1, n):
+		if j == i:
+			continue
+		dist += abs(arr[prev_idx][0] - arr[j][0]) + abs(arr[prev_idx][1] - arr[j][1])
+		prev_idx = j
+	
+	ans = min(ans, dist)
 
-def distance(arr, cnt, value, state_value, state) :
-    if cnt == n - 1:
-        value += abs(state[0] - end_point[0]) + abs(state[1] - end_point[1])
-        ans.append(value)
-        return ans
-    value += abs(state[0] - arr[state_value][0]) + abs(state[1] - arr[state_value][1])
-    cnt += 1
-    state = [arr[state_value][0], arr[state_value][1]]
-    state_value += 1
-    distance(arr, cnt, value, state_value, state)
-
-for i in range(0,n-2) :
-    state_value = i+1
-    cnt = 2
-    value = 0
-    distance(arr, cnt, value, state_value, state)
-    
-print(min(ans))
+# 출력
+print(ans)
