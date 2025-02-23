933b2e775f0894cd528d06064eac0a43db4370d6
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-23 15:19:50 +0900

    Title: 최고의 13위치 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250223/최고의 13위치 2/best-place-of-13-2.py
+++ 250223/최고의 13위치 2/best-place-of-13-2.py
@@ -6,7 +6,7 @@ for i in range(n) :
     for j in range(n-2) :
         for k in range(n) :
             for l in range(n-2) :
-                if i == k and abs(l-j) < 2 :
+                if i == k and abs(l-j) < 3 :
                     continue
 
                 cnt1 = arr[i][j] + arr[i][j+1] + arr[i][j+2]
