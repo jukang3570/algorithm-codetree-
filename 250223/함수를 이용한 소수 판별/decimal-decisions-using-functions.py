1fc08689ccd0f9d4ac89a2ba6d84431fd1f690b5
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-23 15:55:19 +0900

    Title: 함수를 이용한 소수 판별, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250223/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,13 @@
+
+a,b = map(int,input().split())
+cnt = 0
+def check(j) :
+    if j %2 != 0 and j%3 != 0 :
+        return j
+    else :
+        return 0
+
+
+for i in range(a,b+1) :
+    cnt += check(i)
+print(cnt)
