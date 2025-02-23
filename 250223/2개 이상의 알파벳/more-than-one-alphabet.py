5ee7cbb801aaf1a3638eb298664c909525cacc6f
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-02-23 16:42:47 +0900

    Title: 2개 이상의 알파벳, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250223/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,16 @@
+arr = list(input())
+
+def check_str(arr) :
+    for i in range(len(arr)) :
+        cnt = 0
+        for j in range(len(arr)) :
+            if i != j and arr[i] == arr[j] :
+                cnt += 1
+        if cnt >2 :
+            return False
+    return True
+check = check_str(arr)
+
+if check == True :
+    print('Yes')
+else : print('No')
