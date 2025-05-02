e22068dc3025f9edf5b370f2c3f2062240615f28
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-02 16:39:46 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/DateTime to DateTime/datetime-to-datetime.py
+++ 250502/DateTime to DateTime/datetime-to-datetime.py
@@ -4,4 +4,7 @@ a, b, c = map(int, input().split())
 day = (a - 11) * 24 * 60
 hour = (b - 11) * 60
 minute = c - 11
-print(day+hour+minute)
+if a < 11 : print(-1)
+elif a == 11 and b < 11 : print(-1)
+elif a == 11 and b == 11 and c < 11 : print(-1)
+else : print(day+hour+minute)
