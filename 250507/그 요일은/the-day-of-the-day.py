2d3661e9a22f22a75126f108d04fe8733304f0f0
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-07 16:06:05 +0900

    Title: 그 요일은, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250507/그 요일은/the-day-of-the-day.py
+++ 250507/그 요일은/the-day-of-the-day.py
@@ -1,16 +1,17 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
-# Please write your code here.
+# 2024년은 윤년이므로 2월은 29일까지
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-m1_day = num_of_days[:m1]
-m2_day = num_of_days[:m2]
-day1 = sum(m1_day) + d1
-day2 = sum(m2_day) + d2
-tmp = day2 - day1
+
+# 날짜를 일수로 변환
+day1 = sum(num_of_days[:m1]) + d1
+day2 = sum(num_of_days[:m2]) + d2
+
 cnt = 0
-for i in range(tmp) :
-    if day[i%7] == A : cnt += 1
-    
-print(cnt+1)
+for i in range(day1, day2 + 1):
+    if day[i % 7] == A:
+        cnt += 1
+
+print(cnt)
