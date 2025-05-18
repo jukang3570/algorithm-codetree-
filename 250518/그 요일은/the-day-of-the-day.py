0183c431f54bc198e46b4844668c98aae494b28b
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-18 15:41:48 +0900

    Title: 그 요일은, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250518/그 요일은/the-day-of-the-day.py
+++ 250518/그 요일은/the-day-of-the-day.py
@@ -1,17 +1,21 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
-# 윤년 기준
+# 2024년은 윤년이므로 2월 29일까지
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-# 기준일(1월 1일)을 0일로 보고 일수 변환
-start = sum(num_of_days[:m1]) + d1 - 1
-end = sum(num_of_days[:m2]) + d2 - 1
+# 날짜를 일수로 변환 (1월 1일 기준 총일수)
+start = sum(num_of_days[:m1]) + d1
+end = sum(num_of_days[:m2]) + d2
+
+# 기준 요일 인덱스: m1월 d1일이 월요일 == 0번 인덱스
+base_idx = days.index("Mon")
 
 cnt = 0
-for i in range(start, end + 1):
-    if days[i % 7] == A:
+for i in range(end - start + 1):  # (start일부터 end일까지)
+    current_day = days[(base_idx + i) % 7]
+    if current_day == A:
         cnt += 1
 
 print(cnt)
