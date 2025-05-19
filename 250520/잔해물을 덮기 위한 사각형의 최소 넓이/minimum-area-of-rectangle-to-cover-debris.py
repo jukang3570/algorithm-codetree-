fa210b1a473f7935e59685eac9c816e63632f323
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-20 01:43:43 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 211ms, Memory: 64MB, Status: Passed - Codetree

--- 250520/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250520/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -9,17 +9,16 @@ def square(x1, y1, x2, y2, c) :
 
 square(a[0]+1000,a[1]+1000,a[2]+1000,a[3]+1000, '1')
 square(b[0]+1000,b[1]+1000,b[2]+1000,b[3]+1000, '0')
-ans = 0
-wide = 0
-height = 0
+min_x,min_y,max_x,max_y = 2000, 2000, 0, 0
+flag = False
+for i in range(2000) :
+    for j in range(2000) :
+        if grid[i][j] == '1' :
+            flag = True
+            min_x = min(min_x, i)
+            min_y = min(min_y, j)
+            max_x = max(max_x, i)
+            max_y = max(max_y, j)
 
-for i in range (2001) :
-    w_cnt = 0
-    h_cnt = 0
-    for j in range (2001) :
-        if grid[i][j] == '1' : w_cnt += 1
-        if grid[j][i] == '1' : h_cnt += 1
-    wide = max(w_cnt, wide)
-    height = max(h_cnt, height)
-
-print(wide * height)
+if flag == False : print(0)
+else : print ( (max_x - min_x + 1) * (max_y - min_y + 1) )
