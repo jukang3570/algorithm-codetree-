165aeaee332e50da23b120dbc9455ee557d76309
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-19 18:28:05 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250519/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 250519/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -6,6 +6,10 @@ def start_print(n) :
     print('* ' * n)
     start_print(n-1)
 
-
+def start_print_reverse(s, n) :
+    if s > n : return
+    print('* ' * s)
+    start_print_reverse(s+1,n)
 
 start_print(n)
+start_print_reverse(1, n)
