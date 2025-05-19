abb6019237883f214cd0051da4e360050bb3917d
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-19 22:02:42 +0900

    Title: 신기한 타일 뒤집기, Time: 97ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250519/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,25 @@
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+state = [ '0' for _ in range(101)]
+# Please write your code here.
+pos = 0
+for i in range(n) :
+    d = dir[i]
+    y = x[i]
+    if d == 'R' :
+        for j in range(y) :
+            state[pos] = 'B'
+            pos += 1
+        pos -= 1
+    elif d == 'L' :
+        for j in range(y) :
+            state[pos] = 'W'
+            pos -= 1
+        pos += 1
+
+print(state.count('W'), state.count('B'))
