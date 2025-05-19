7bfe8fe348b35a01b2e6d36f97309d7f86f223d5
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-05-19 22:46:45 +0900

    Title: 사각형의 총 넓이 2, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250519/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 250519/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,20 +1,35 @@
+OFFSET = 100
+MAX_R = 200
+
+# 변수 선언 및 입력
 n = int(input())
-x1, y1, x2, y2 = [], [], [], []
-for _ in range(n):
-    a, b, c, d = map(int, input().split())
-    x1.append(a)
-    y1.append(b)
-    x2.append(c)
-    y2.append(d)
+rects = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-# Please write your code here.
-grid = [ ['0' for _ in range(300)] for _ in range(300)]
+checked = [
+    [0] * (MAX_R + 1)
+    for _ in range(MAX_R + 1)
+]
 
-for i in range(2) :
-    for j in range(x1[i],x2[i]) :
-        for k in range(y1[i],y2[i]) :
-            grid[j+100][k+100] = '1'
+for x1, y1, x2, y2 in rects:
+    # OFFSET을 더해줍니다.
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
+    
+    # 직사각형을 칠해줍니다.
+    # 격자 단위로 진행하는 문제이므로
+    # x2, y2에 등호가 들어가지 않음에 유의합니다.
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            checked[x][y] = 1
 
-# 마지막에
-ans = sum(row.count('1') for row in grid)
-print(ans)
+# 직사각형 넓이의 총 합을 구합니다.
+area = 0
+for x in range(0, MAX_R + 1):
+    for y in range(0, MAX_R + 1):
+        if checked[x][y]:
+            area += 1
+
+print(area)
