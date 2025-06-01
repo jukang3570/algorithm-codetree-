6310f67545d52ce3be7a34af6892498ac55bf2f7
Ganggang_gang <64056034+jukang3570@users.noreply.github.com>
2025-06-01 16:09:00 +0900

    Title: 선두를 지켜라, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250601/선두를 지켜라/keep-the-lead.py
+++ 250601/선두를 지켜라/keep-the-lead.py
@@ -1,6 +1,6 @@
 n,m = map(int,input().split())
 first = 0
-ans = 0
+ans = -1
 pos_a = 0
 pos_b = 0
 a = []
@@ -18,10 +18,12 @@ for _ in range(m) :
         pos_b += v
         b.append(pos_b)
 
-if a[0] > b[0] : first = a
-else : first = b
 
 for i in range(1,len(a)) :
+    if ans < 0 :
+        if a[i] > b[i] : first = a
+        elif b[i] > a[i] : first = b
+        ans += 1
     if first == a :
         if b[i] > a[i] : 
             ans += 1
